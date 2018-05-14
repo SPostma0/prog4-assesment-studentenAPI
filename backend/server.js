0aecd1f33b9394e21c7f08467eefbdfab3b56714
SPostma <s.postma1994@gmail.com>
2018-05-14 17:12:36 +0200

    Implementing protected api

--- backend/server.js
+++ backend/server.js
@@ -63,15 +63,24 @@ app.use('*', function (req, res, next){
 
 
 
+
 ////////SECURITY///////////////
-app.get('api/protected', ensureToken, function(req, res)){
-    
-}
-
+app.get('/api/protected', ensureToken, (req, res) =>{
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        res.json({
+          description: 'Protected information. Congrats!'
+        });
+      }
+    });
+  })
+  
 ////////ROUTE CONNECTIONS//////
-
 router.post('/register', RegisterRouter.register);
 router.post('/login', LoginRouter.login)
+
 
 app.use('/api', router);
 
@@ -95,3 +104,15 @@ app.listen(PORT, () => {
 
 
 
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[1];
+      req.token = bearerToken;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
