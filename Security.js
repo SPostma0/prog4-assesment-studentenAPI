5751e738162bc4eda5eac1f868106629a964560b
SPostma <s.postma1994@gmail.com>
2018-05-15 17:13:04 +0200

    Student working better with AWT
    Studenthuis routes are now using uid from AWT

--- Security.js
+++ Security.js
@@ -12,3 +12,48 @@ exports.ensureToken = function ensureToken(req, res, next) {
       res.sendStatus(401);
     }
   }
+
+
+  exports.decodeToken =  function url_base64_decode(req, res, next) {
+
+    var atob = require('atob');
+
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[1];
+
+      str = bearerToken;
+      
+    //  console.log("Dit is le token: "+ bearerToken);
+    }
+
+    var output = str.replace(/-/g, '+').replace(/_/g, '/');
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw 'Illegal base64url string!';
+    }
+    var result = atob(output); 
+    
+    try{
+     return decodeURIComponent(escape(result));
+    } catch (err) {
+     // console.log(result);
+      
+     tokenArray =  result.split('{');
+
+     phase1 = tokenArray[3];
+     phase2 = '{' + phase1.split('}')[0] + '}' 
+      return phase2;
+    }
+  }
+
+
